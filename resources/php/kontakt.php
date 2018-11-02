72bf32fbfebaf32333f04ce9f51760f56755ab28
yannik <yannik@golem>
2018-11-02 01:07:47 +0100

    tried it failed

--- resources/php/kontakt.php
+++ resources/php/kontakt.php
@@ -2,6 +2,7 @@
     $email = $_POST["email"];
     $name = $_POST["name"];
     $message = $_POST["message"];
+    $surname = $_POST["surname"];
     $errormessage = $err = "";
     $betreff = $_POST["betreff"];
     
@@ -31,21 +32,35 @@
         }
     }
     
+    function errmessage($name, $field){
+        if ($field == "Email"){
+            return $name . " ist keine gültige Emailadresse!";
+        } else {
+            return $name . " ist kein gültiger " . $field;
+        }
+    }
     #Main
-    
+    $namebool = correctname($name);
+    $surnamebool = correctname($surname);
+    $emailbool = correctemail($email);
     if (isempty($email)){
         $errormessage = showmessage("Email");
     } elseif (isempty($name)){
-        $errormessage = showmessage("Name");
+        $errormessage = showmessage("Name"); 
+    } elseif (isempty($name)){
+        $errormessage = showmessage("Vorname");
     } elseif (isempty($message)) {
         $errormessage = showmessage("Nachricht");
-    } elseif (!empty(correctname($name))){
-        $err = correctname($name)
-    } elseif (!empty(correctemail($email))){
-        correctemail($email)
+    } elseif (!empty($namebool)){
+        $err = errmessage($name, "Name");
+    } elseif (!empty($surnamebool)){
+        $err = errmessage($surname, "Vorname");
+    } elseif (!empty($emailbool)){
+        $err = errmessage($email, "Emailadresse");
     }
     
     $empfaenger = "testytest1234@mail1a.de";
-    $from = "From: " . $name . " <" . $email . ">";
-    mail($empfaenger, $betreff, $text, $from);
+    $from = "From: " . $name . " " . $surname ." <" . $email . ">";
+    $mail = mail($empfaenger, $betreff, $text, $from);
+    
 ?>
